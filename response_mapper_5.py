e606ae95363662613a61f3578e04b163a7bf67da
katelclancy <katelclancy@gmail.com>
2025-02-26 21:44:05 -0500

    Add files via upload

---
+++ response_mapper_5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#"""
+#Created on Tue Feb 25 16:05:35 2025
+
+#@author: KateClancy
+#"""
+
+import sys
+import re
+
+def main(argv):
+    pattern = re.compile(r'"\s(\d)\d{2}\s')
+    for line in sys.stdin:
+        if not line:
+            continue
+        match = pattern.findall(line)
+        if match:
+            for word in match:
+                response_cat = str(word)
+                print(f"{response_cat}**\t1")
+
+if __name__ == "__main__":
+    main(sys.argv)
