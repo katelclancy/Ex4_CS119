da0bcf540ee35c3cae77fbe1475b21e06fec6846
katelclancy <katelclancy@gmail.com>
2025-02-26 20:59:35 -0500

    Add files via upload

---
+++ ip_mapper_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#"""
+#Created on Tue Feb 25 16:05:35 2025
+
+#@author: KateClancy
+#"""
+
+import sys
+import re
+
+def main(argv):
+    pattern = re.compile(r'^(\d+\.\d+\.\d+\.\d+)')
+    for line in sys.stdin:
+        for word in pattern.findall(line):
+            print(word + "\t1")
+
+if __name__ == "__main__":
+    main(sys.argv)
